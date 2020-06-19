7b874c52e1032553e4e80e3b6f29a202b967c946
hyun <rlaalgus132@naver.com>
2020-06-19 19:33:59 +0900

    init commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { Component, useState } from 'react';
+import {StyleSheet,Text,View, FlatList} from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+
+export {App};
+
+export default function App(){
+
+  const [todos, setTodos] = useState([
+    {text:'buy coffe', key:'1'},
+    {text:'create an app', key:'2'},
+    {text:'play on the switch',key:'3'}
+  ]);
+
+  const pressHandler = (key) =>{
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  return(
+    <View style={styles.container}>
+      <Header/>
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item})=>(
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#FFF",
+  },
+  content:{
+    padding: 40,
+
+  },
+  list : {
+    marginTop:20,
+  }
+})
